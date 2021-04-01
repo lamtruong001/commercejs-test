fd1166162bbdda7c1b5de36aa7b25a55d10b68f9
Lam <l.truong@rgu.ac.uk>
2021-04-01 09:58:51 +0200

    models renamed + footer links

--- components/Footer.js
+++ components/Footer.js
@@ -10,9 +10,9 @@ function Footer() {
       <div className="container mx-auto px-3 md:px-4 lg:px-5 md:flex md:items-center space-y-6 md:space-y-0">
       <div class= "m-8 md:hidden"/>
         <div className="text-center md:text-left w-full md:w-1/2 space-x-0 md:space-x-8">
-        <a class="text-lg font-medium transition duration-200 ease-in-out hover:text-blue" href="/">About</a>
+        <a class="text-lg font-medium transition duration-200 ease-in-out hover:text-blue" href="#">About</a>
         <div class= "m-8 md:hidden"/>
-        <a class="text-lg font-medium transition duration-200 ease-in-out hover:text-blue" href="/">Return Policy</a>
+        <a class="text-lg font-medium transition duration-200 ease-in-out hover:text-blue" href="#">Return Policy</a>
         
         </div>
         
@@ -20,17 +20,17 @@ function Footer() {
         <div class= "m-8 md:hidden"/>
           <div className="text-center md:text-right space-x-16 md:space-x-4">
             <a
-              className="" href="/"
+              className="" href="#"
             >
               <Ig className="h-6 w-6 inline fill-current text-black transition duration-200 ease-in-out hover:text-blue"/>
             </a>
             <a
-              className="" href="/"
+              className="" href="#"
             >
               <Fb className="h-6 w-6 inline fill-current text-black transition duration-200 ease-in-out hover:text-blue"/>
             </a>
             <a
-              className="" href="/"
+              className="" href="#"
             >
               <Tw className="h-6 w-6 inline fill-current text-black transition duration-200 ease-in-out hover:text-blue" />
             </a>
